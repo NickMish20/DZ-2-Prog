cfefd84370a1db706835ae7e01a3a89eda41cc8e
Your Name <you@example.com>
2022-12-24 14:45:48 +0300

    DZ#2

---
+++ zadach_13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+    Console.Clear();
+    Console.Write("Введите целое число, лучше трехзначное и больше: ");
+    int number = int.Parse(Console.ReadLine());
+    int number1 = number;
+    int n=0;
+    while (number1>0)
+      {
+        number1 /=10;
+        n++;
+      }
+    Console.Write("А теперь магия, третья слева цифра Вашего чиса: ");
+    int x=Convert.ToInt32(Math.Pow(10,n-3));
+    if (n<3)
+      {
+    Console.Write("А вот и нет... Третьей цифры в Вашем числе нет :(");        
+      }
+    else
+      {
+    Console.Write($"{(number/x)%10}");
+      }
---
+++ zadach_15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+    Console.Clear();
+    Console.Write("Введите целое число от 1 до 7: ");
+    int number = int.Parse(Console.ReadLine());
+    if (number<1 || number>7)
+    {
+      Console.Write("Вы ввели  число не от 1 до 7");
+    }
+    else
+    {
+    if(6<=number && number <=7)
+      {
+    Console.Write("Да");      
+      }
+      else
+      {
+    Console.Write("Нет");          
+      }
+    }
+    
